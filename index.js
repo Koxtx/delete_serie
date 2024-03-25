9447dea5aa13a23aa40646df810c7cb9e51c4106
Koxtx <corentin.lavarde@gmail.com>
2024-03-25 16:10:31 +0100

    push delete serie

---
+++ index.js
@@ -0,0 +1,71 @@
+const series = [
+  {
+    name: "Breaking Bad",
+    seen: false,
+  },
+  {
+    name: "The Wire",
+    seen: true,
+  },
+];
+
+const seriesContainer = document.querySelector("ul");
+const input = document.querySelector("input");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const value = input.value;
+  input.value = "";
+  addSerie(value);
+});
+
+const displaySerie = () => {
+  const seriesNode = series.map((serie, index) => {
+    return createSerie(serie, index);
+  });
+  seriesContainer.innerHTML = "";
+  seriesContainer.append(...seriesNode);
+};
+
+function createSerie(serie, index) {
+  const liste = document.createElement("li");
+
+  const span = document.createElement("Span");
+  span.classList.add("todo");
+  if (serie.seen) {
+    span.classList.add("done");
+  }
+
+  const parag = document.createElement("p");
+  parag.innerText = serie.name;
+
+  const btnEdit = document.createElement("button");
+  btnEdit.innerText = "Editer";
+
+  const btnDelete = document.createElement("button");
+  btnDelete.classList.add("delete");
+  btnDelete.innerText = "Supprimer";
+  btnDelete.addEventListener("click", () => {
+    deleteSerie(index);
+  });
+
+  const btnAjout = document.querySelector(".ajout");
+
+  liste.append(span, parag, btnEdit, btnDelete);
+
+  return liste;
+}
+
+const addSerie = (value) => {
+  series.push({ name: value, seen: false });
+  displaySerie();
+};
+
+const deleteSerie = (index) => {
+  console.log(index);
+  series.splice(index, 1);
+  displaySerie();
+};
+
+displaySerie();
